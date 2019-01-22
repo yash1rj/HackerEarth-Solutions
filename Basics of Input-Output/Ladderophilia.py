ffa45dcff9a7c2ddfb301fad2d9f9b09a56ce7f4
yash1rj <yashrj130@gmail.com>
2019-01-22 15:30:58 +0530

    Create Ladderophilia.py

---
+++ Basics of Input-Output/Ladderophilia.py
@@ -0,0 +1,44 @@
+'''
+Aditya is fond of ladders. Everyday he goes through pictures of ladders online but 
+unfortunately today he ran out of ladder pictures online. Write a program to print “ladder with N steps”, 
+which he can use to get his daily dose of ladder love.
+
+INPUT:
+Input contains integer N, the number of steps in the ladder
+
+OUTPUT:
+
+Print the ladder with the gap between two side rails being 3 spaces(“   “).
+Check the sample output for format of printing the ladder.
+
+CONSTRAINTS:
+1<=N<=40
+
+SAMPLE INPUT 
+4
+SAMPLE OUTPUT 
+*   *
+*   *
+*****
+*   *
+*   *
+*****
+*   *
+*   *
+*****
+*   *
+*   *
+*****
+*   *
+*   *
+
+'''
+# Write your code here
+step=int(input())
+
+for i in range(step):
+    print("*   *")
+    print("*   *")
+    print("*****")
+print("*   *")
+print("*   *")
