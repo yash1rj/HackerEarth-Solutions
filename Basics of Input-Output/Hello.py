72cb92578b944536335772ec5ca8eaba89421434
yash1rj <yashrj130@gmail.com>
2019-01-22 15:36:30 +0530

    Create Hello.py

---
+++ Basics of Input-Output/Hello.py
@@ -0,0 +1,10 @@
+'''
+Kirti is new to programming and she doesn't know how to code. 
+Can you write a code, which would print "Hello Kirti" on the screen.
+
+SAMPLE OUTPUT 
+Hello Kirti
+'''
+
+# Write your code here
+print('Hello Kirti')
