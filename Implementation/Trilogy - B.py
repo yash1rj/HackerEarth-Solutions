2dd0d5df51ea9cf8bdfaf10d59935c39538e065c
yash1rj <yashrj130@gmail.com>
2019-01-29 18:26:33 +0530

    Create Trilogy - B.py

---
+++ Implementation/Trilogy - B.py
@@ -0,0 +1,16 @@
+'''
+Gupta, Chaturvedi and Saxena are very close friends and they are on a Goa trip. One night, 
+they went to a famous bar of Goa. In the bar they saw that beer bottles were arranged in a long row. 
+Every beer bottle had some price tag and these prices followed arithmetic progression. 
+Gupta who was standing near the 116th bottle said that the total price of all the bottles upto 116th bottle 
+is Rs. 220922 while Chaturvedi who was standing near 343rd bottle said that the total price of all the beer 
+bottles upto 343rd bottle is Rs. 1937950. Now they asked Saxena what will be the total price of all the beer 
+bottles upto the 11631th bottle ?
+
+Output the total price of all the bottles upto 11631th bottle.
+
+Compile and Test can give Wrong Answer. Once you have written your answer, press Submit button to check your result.
+'''
+
+# Write your code here
+print("2232012162")
