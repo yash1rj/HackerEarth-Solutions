66f9d3257e0276bb898d23759b32ebf57ce2118a
Yash Raj <yashrj130@gmail.com>
2019-02-06 18:00:50 +0530

    Create Trilogy - A.py

---
+++ Implementation/Trilogy - A.py
@@ -0,0 +1,14 @@
+'''
+Gupta, Chaturvedi and Saxena are very close friends and they are on a Goa trip. 
+On one fine morning all three of them went to a beach with a timer and observed the timing of the arriving sea waves. 
+Since all three have excellent observation skills, Gupta observed that 5th wave arrived at 178th second and then Chaturvedi 
+observed that 17th wave arrived at 646th second and also observed that waves followed arithmetic progression. 
+Now they asked Saxena when will 931th wave will arrive ?
+
+Print the time when 931th wave will arrive.
+
+To see how to submit solution please check this link
+Compile and Test can give Wrong Answer. Once you have written your answer, press Submit button to check your result.
+'''
+
+print(930*39+22)
